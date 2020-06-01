30badb1e970422a2fd5f0fe929b6490a9a464543
Doug Ritchie <dritchie3@sbcglobal.net>
2020-06-01 11:30:23 -0500

    Finished JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Navbar from './components/Navbar'
+import Table from "./components/Table"
+// where I learned about context https://www.youtube.com/watch?v=lhMKvyLRWo0
+import { EmployeeContext } from "./components/EmployeeContext"
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [displayedEmployees, setDisplayedEmployees] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}>
+        <Navbar />
+        <Table />
+      </EmployeeContext.Provider>
+    </div>  
   );
 }
 
